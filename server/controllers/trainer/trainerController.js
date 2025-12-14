707d959258bb6a607e6912c71905efda70368438
animeshthakur7879 <animeshthakur7879@gmail.com>
2025-12-14 15:26:35 +0530

    trainer Controller

--- server/controllers/trainer/trainerController.js
+++ server/controllers/trainer/trainerController.js
@@ -4,7 +4,12 @@ const Plan = require('../../Models/planModel');
 
 const getAllPlans = expressAsyncHandler(async (req, res) => {
     // Logic to get all plans for trainers
-    res.status(200).json({ message: "Get all plans for trainers" });
+    const plans = await Plan.find();
+    if(plans.length === 0){
+        res.status(404);
+        throw new Error("No plans found");
+    }
+    res.status(200).json(plans);
 });
 
 const createPlan = expressAsyncHandler(async (req, res) => {
@@ -29,12 +34,57 @@ const createPlan = expressAsyncHandler(async (req, res) => {
         throw new Error("Plan not created");
     }
 
+    res.status(201).json(plan);
 
-
-    
 }); 
+
+const deletePlan = expressAsyncHandler(async (req, res) => {
+    // Logic to delete a plan
+    const planId = req.params.pid;
+    const plan = await Plan.findByIdAndDelete(planId);
+    if(!plan){
+        res.status(404);
+        throw new Error("Plan not found");
+    }
+
+    res.status(200).json({
+        message: "Plan deleted successfully" , 
+        id : planId
+    })
+
+    // res.status(200).json({ message: "Delete a plan" });
+});
+
+const updatePlan = expressAsyncHandler(async (req, res) => {
+    // Logic to update a plan
+    const planId = req.params.pid;
+    // Update logic here    
+    const updatedPlan = await Plan.findByIdAndUpdate(planId, req.body, { new: true });
+    if(!updatedPlan){
+        res.status(404);
+        throw new Error("Plan not found");
+    }   
+
+    res.status(200).json({ message: "Update a plan" , plan : updatedPlan});
+});
+
+const getTrainerPlans = expressAsyncHandler(async (req, res) => {
+    const trainerId = req.params.tid;
+    // console.log(trainerId);
+    const plans = await Plan.find({ trainer: trainerId });  
+
+    if(!plans){
+        res.status(404);
+        throw new Error("No plans found for this trainer");
+    }   
+
+    res.status(200).json(plans);
+})
 
 module.exports = {
     getAllPlans,
-    createPlan
+    createPlan ,
+    deletePlan ,
+    updatePlan , 
+    getTrainerPlans
 };
--- server/routes/trainer/trainerRoutes.js
+++ server/routes/trainer/trainerRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { getAllPlans, createPlan } = require('../../controllers/trainer/trainerController');
+const { getAllPlans, createPlan, deletePlan, updatePlan, getTrainerPlans } = require('../../controllers/trainer/trainerController');
 const trainer = require('../../Middlewares/trainerMiddleware');
 
 const router = express.Router();
 
 router.get("/"  , getAllPlans)
+router.get("/:tid"  , getTrainerPlans)
 router.post("/" , trainer , createPlan)
+router.delete("/:pid" , trainer , deletePlan)
+router.put("/:pid" , trainer , updatePlan)
 
 
 
